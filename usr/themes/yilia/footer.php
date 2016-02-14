72010991492c25a4fc75dae337749f535a116485
wujunze <itwujunze@163.com>
2016-02-14 22:56:57 +0800

    增加网站运行时间的JS代码

--- usr/themes/yilia/footer.php
+++ usr/themes/yilia/footer.php
@@ -1,7 +1,8 @@
-
-    <footer id="footer">
+<footer id="footer" xmlns="http://www.w3.org/1999/html">
       <div class="outer">
         <div id="footer-info">
+          <div><SPAN id=span_dt_dt></SPAN></div>
+          <br>
           <div class="footer-left">
             &copy;  <?php echo date('Y');?>  <a href="<?php $this->options->siteurl(); ?>"><?php $this->options->title(); ?></a>|<a href="" id="StranLink">繁体中文</a>|<a href="http://www.miitbeian.gov.cn/" rel="nofollow"  target="_blank">京ICP备16000939号</a>
           </div>
@@ -26,7 +27,28 @@
 		prettify: true,
 		base_url: "<?php $this->options->themeUrl();?>"
 	}</script>
-  <script src="<?php $this->options->themeUrl(); ?>js/require.min.js" type="text/javascript"></script>
+    <SCRIPT language=javascript>
+      function show_date_time(){
+        window.setTimeout("show_date_time()", 1000);
+        BirthDay=new Date("09/14/2015 00:00:00");//这个日期是可以修改的
+        today=new Date();
+        timeold=(today.getTime()-BirthDay.getTime());
+        sectimeold=timeold/1000
+        secondsold=Math.floor(sectimeold);
+        msPerDay=24*60*60*1000
+        e_daysold=timeold/msPerDay
+        daysold=Math.floor(e_daysold);
+        e_hrsold=(e_daysold-daysold)*24;
+        hrsold=Math.floor(e_hrsold);
+        e_minsold=(e_hrsold-hrsold)*60;
+        minsold=Math.floor((e_hrsold-hrsold)*60);
+        seconds=Math.floor((e_minsold-minsold)*60);
+        span_dt_dt.innerHTML="博客已运行"+daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒";
+      }
+      show_date_time();
+    </SCRIPT>
+
+    <script src="<?php $this->options->themeUrl(); ?>js/require.min.js" type="text/javascript"></script>
   <script src="<?php $this->options->themeUrl(); ?>js/jquery.min.js" type="text/javascript"></script>
   <script src="<?php $this->options->themeUrl(); ?>js/main.js" type="text/javascript" ></script>
   <script src="<?php $this->options->themeUrl(); ?>js/number.js" type="text/javascript" ></script>
