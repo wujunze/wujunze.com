1aa1f6d57eba82871ac50b5aa4205349b202a1e4
wujunze <itwujunze@163.com>
2016-02-16 19:15:53 +0800

    增加Ctrl+C事件的特效🔥

--- usr/themes/yilia/footer.php
+++ usr/themes/yilia/footer.php
@@ -61,6 +61,36 @@
             $("#web-icon").attr('href',"<?php $this->options->siteUrl(); ?>favicon.ico");
          }
         };
+        document.body.addEventListener('copy', function (e) {
+            if (window.getSelection().toString() && window.getSelection().toString().length > 42) {
+                setClipboardText(e);
+                alert('商业转载请联系作者获得授权，非商业转载请注明出处，谢谢合作。');
+            }
+        });
+        function setClipboardText(event) {
+            var clipboardData = event.clipboardData || window.clipboardData;
+            if (clipboardData) {
+                event.preventDefault();
+
+                var htmlData = ''
+                    + '著作权归作者所有。<br>'
+                    + '商业转载请联系作者获得授权，非商业转载请注明出处。<br>'
+                    + '作者：Wujunze<br>'
+                    + '链接：' + window.location.href + '<br>'
+                    + '来源：wujunze.com<br><br>'
+                    + window.getSelection().toString();
+                var textData = ''
+                    + '著作权归作者所有。\n'
+                    + '商业转载请联系作者获得授权，非商业转载请注明出处。\n'
+                    + '作者：Wujunze\n'
+                    + '链接：' + window.location.href + '\n'
+                    + '来源：wujunze.com\n\n'
+                    + window.getSelection().toString();
+
+                clipboardData.setData('text/html', htmlData);
+                clipboardData.setData('text/plain',textData);
+            }
+        }
     </script>
   <script src="<?php $this->options->themeUrl(); ?>js/require.min.js" type="text/javascript"></script>
   <script src="<?php $this->options->themeUrl(); ?>js/jquery.min.js" type="text/javascript"></script>
