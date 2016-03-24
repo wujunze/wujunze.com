69c07b44e52fea1dcadbc319ee19ed6a68ad1aba
wujunze <itwujunze@163.com>
2016-03-24 16:31:11 +0800

    增加QQ加群链接

--- usr/themes/yilia/post.php
+++ usr/themes/yilia/post.php
@@ -26,6 +26,12 @@
             <?php $this->content(''); ?>
         </div>
     </div>
+    <div class="qq_box">
+        让学习成为一种习惯! 欢迎加入IT技术交流群213470752
+        <a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=4312a8b3aaae6d87ef30798db32357f8b35c9c084c5c51f858d90a324ae4ed30">
+            <img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="IT技术交流" title="IT技术交流">
+        </a>
+    </div>
     <nav id="article-nav">
         <?php thePrev($this);?>
         <?php theNext($this);?>
